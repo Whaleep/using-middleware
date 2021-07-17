e45c76ce78d388a081a702058f6b36a877ef70f3
Whaleep <billy_1817@yahoo.com.tw>
2021-07-18 05:12:28 +0800

    feat: add views for switch page

---
+++ README.md
@@ -0,0 +1,35 @@
+# Middleware 實作練習 Using Middleware
+實作在 Node/Express 開發中，加入一支 middleware，將所有需求回應記錄在 server log 裡。
+
+## 特色 Features
+- 在伺服器接收請求時記錄時間、URL 和 請求HTTP方法
+- 在伺服器回應請求時記錄時間、URL、請求HTTP方法 和伺服器處理時間
+
+
+## 環境建置 Environment Setup
+
+1. nvm & Node.js
+2. nodemon
+
+## 安裝 Installing
+
+1. 在終端機輸入指令 Clone 此專案至本機電腦
+```
+git clone https://github.com/Whaleep/using-middleware
+```
+2. 進入專案目錄
+```
+cd using-middleware
+```
+3. 安裝相關套件
+```
+npm install
+```
+4. 啟動專案
+```
+npm run dev
+```
+5. 出現以下訊息後，即可在 http://localhost:3000 開始使用
+```
+App running on port:3000
+```
--- app.js
+++ app.js
@@ -1,26 +1,17 @@
 const express = require('express')
+const exphbs = require('express-handlebars')
+const routes = require('./routes')
 const app = express()
 const port = 3000
+
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
+app.set('view engine', 'hbs')
 
 const { conutByDate, conutByHrtime } = require('./tools/general')
 app.use(conutByHrtime)
 app.use(conutByDate)
 
-app.get('/', (req, res) => {
-  res.send('列出全部 Todo')
-})
-
-app.get('/new', (req, res) => {
-  res.send('新增 Todo 頁面')
-})
-
-app.get('/:id', (req, res) => {
-  res.send('顯示一筆 Todo')
-})
-
-app.post('/', (req, res) => {
-  res.send('新增一筆  Todo')
-})
+app.use(routes)
 
 app.listen(port, () => {
   console.log(`App running on port ${port}`)
